f5f74cc23eae5cc2ac48a94e785ed3a479fd4360
zldavis <zldavis@gmail.com>
2014-11-07 14:41:01 -0800

    Initial implementation

--- README.md
+++ README.md
@@ -1,4 +1,68 @@
-dirty-cache
-===========
+# dirty-cache
 
-Simple wrapper around lru-cache that uses pub-sub for notifying of a dirty entry.
+Basic wrapper around lru-cache that uses pub-sub for notifying of a
+dirty entry.  Not all lru-cache functionality is exposed, mainly
+get, set, and del.
+
+One difference to be aware of, values in cache are a deep clone of
+the inserted value so that any modifications made to the object
+before or after being cached don't change modify the object in
+cache.
+
+```javascript
+var obj = {foo: 'bar'};
+cache.set(1, obj);
+
+obj == cache.get(1); // false
+obj.foo == cache.get(1).foo; // true
+```
+
+## Usage
+
+```javascript
+var dirtyCache = require('dirty-cache');
+
+var cache = dirtyCache({name: 'users', max: 100, maxAge: 60000});
+
+cache.set('0087e434-9dfe-4ac9-99e9-fbdbd9f834f5', {name: 'TJ'});
+cache.get('0087e434-9dfe-4ac9-99e9-fbdbd9f834f5'); // {name: 'TJ'}
+```
+
+## Options
+
+* `name` The name of this cache. Needed when using same cache on
+  distributed system.  Optional or not needed when used as a
+  standalone cache.
+* `max` The maximum number of objects in the cache, Not setting this
+  is kind of silly, since that's the whole purpose of this lib, but
+  it defaults to `Infinity`.
+* `maxAge` Maximum age in ms.  Items are not pro-actively pruned out
+  as they age, but if you try to get an item that is too old, it'll
+  drop it and return undefined instead of giving it to you.
+
+## API
+
+### set(key, value, dirty)
+
+Sets the value of `key` in the local cache. If `dirty`, notifies
+other caches their value is stale.
+
+### get(key)
+
+Gets the latest `value` of `key`, or `undefined` if was not present
+in local cache or has been evicted due to dirtiness or expiration.
+
+### del(key)
+
+Deletes a key out of the local cache and notifies distributed
+caches to evict key.
+
+## Events
+
+### "error"
+
+Error event in the case of connection errors to redis.
+
+### "ready"
+
+Client will emit `ready` after the conenction to redis has been successfully established.
---
+++ test/test.js
@@ -0,0 +1,172 @@
+var assert = require('assert');
+
+var dirtyCache = require('../lib/dirty-cache');
+
+describe('dirty-cache', function() {
+
+    describe('basic', function() {
+
+        var cache;
+        beforeEach(function() {
+            cache = dirtyCache({name: 'users', max: 5, maxAge: 10});
+        });
+
+        it('should be initialized correctly', function() {
+            assert.equal(cache.name, 'users');
+            assert.equal(cache.max, 5);
+            assert.equal(cache.maxAge, 10);
+            assert.equal(cache.length, 0);
+            assert.equal(cache.hits, 0);
+            assert.equal(cache.misses, 0);
+        });
+
+        it('should cache correctly', function() {
+            cache.set('key', 'value');
+            assert.equal(cache.get('key'), 'value');
+            assert.equal(cache.get('nada'), undefined);
+            assert.equal(cache.length, 1);
+        });
+
+        it('should be a deep clone', function() {
+            var value = {a: 1, b: 2};
+            cache.set('key', value);
+            assert.notEqual(cache.get('key'), value);
+            assert.deepEqual(cache.get('key'), value);
+        });
+
+        it('should not be modified in cache', function() {
+            var value = {a: 1, b: 2};
+            cache.set('key', value);
+
+            value.a = 2;
+            assert.notDeepEqual(cache.get('key'), value);
+        });
+
+        it('should evict item after max age', function(done) {
+            cache.set('key', 'value');
+            setTimeout(function() {
+                assert.equal(cache.get('key'), 'value');
+                assert.equal(cache.length, 1);
+            }, 5);
+            setTimeout(function() {
+                assert.equal(cache.get('key'), undefined);
+                assert.equal(cache.length, 0);
+                done();
+            }, 11);
+        });
+
+        it('should evict the least recently used', function() {
+            for (var i=0; i<5; i++) {
+                cache.set(i, i);
+            }
+
+            for (var i=0; i<5; i++) {
+                assert.equal(cache.get(i), i);
+            }
+
+            cache.set(5, 5);
+            assert.equal(cache.get(0), undefined);
+            assert.equal(cache.length, 5);
+
+            for (var i=1; i<6; i++) {
+                assert.equal(cache.get(i), i);
+            }
+        });
+
+        it('should have correct stats', function() {
+            cache.set('key', 'value');
+            assert.deepEqual(cache.stats(), {hits: 0, misses: 0});
+
+            cache.get('key');
+            cache.get('key');
+            assert.deepEqual(cache.stats(), {hits: 2, misses: 0});
+
+            cache.get('nada');
+            assert.deepEqual(cache.stats(), {hits: 2, misses: 1});
+        });
+    });
+
+    describe('cluster', function() {
+
+        var cache1;
+        var cache2;
+        beforeEach(function(done) {
+            var readyCount = 0;
+
+            cache1 = dirtyCache({
+                name: 'users',
+                max: 5,
+                maxAge: 50,
+                redis: {
+                    host: 'localhost',
+                    port: 6379
+                }
+            });
+            cache1.on('error', function(err) {
+                throw err;
+            });
+            cache1.on('ready', function() {
+                if (++readyCount == 2) {
+                    done();
+                }
+            });
+
+            cache2 = dirtyCache({
+                name: 'users',
+                max: 5,
+                maxAge: 100,
+                redis: {
+                    host: 'localhost',
+                    port: 6379
+                }
+            });
+            cache2.on('error', function(err) {
+                throw err;
+            });
+            cache2.on('ready', function() {
+                if (++readyCount == 2) {
+                    done();
+                }
+            });
+
+            setTimeout(function() {
+                if (readyCount < 2) {
+                    throw new Error('unable to connect to redis at localhost:6379 after 5 seconds');
+                }
+            }, 5000);
+        });
+
+        it('should evict the dirty cache on other clients if set dirty', function(done) {
+            cache1.set('key', 'value');
+            cache2.set('key', 'value');
+
+            assert.equal(cache1.get('key'), 'value');
+            assert.equal(cache2.get('key'), 'value');
+
+            cache1.set('key', 'value dirty', true);
+
+            setTimeout(function() {
+                assert.equal(cache1.get('key'), 'value dirty');
+                assert.equal(cache2.get('key'), undefined);
+                done();
+            }, 10);
+        });
+
+        it('should evict the dirty cache on other clients if deleted', function(done) {
+            cache1.set('key', 'value');
+            cache2.set('key', 'value');
+
+            assert.equal(cache1.get('key'), 'value');
+            assert.equal(cache2.get('key'), 'value');
+
+            cache1.del('key');
+
+            setTimeout(function() {
+                assert.equal(cache1.get('key'), undefined);
+                assert.equal(cache2.get('key'), undefined);
+                done();
+            }, 10);
+        });
+
+    });
+});
